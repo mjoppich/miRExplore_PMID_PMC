85d4c683e98bcea3ff63b95bf06badec9f44df74
mjoppich <github@mjoppich.net>
2022-06-15 19:07:38 +0200

    changes for pmid

--- runTextmine.sh
+++ runTextmine.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/sh
 
 MIREXPLORE_PATH=/mnt/f/dev/git/miRExplore/python/
-bash doTextmine.sh "/usr/bin/python3 $MIREXPLORE_PATH/textmining/textmineDocument.py" "./pmc/" "./results.pmc.raw/" "./"
 
-#bash doTextmine.sh "./pmid/pubmed20n" "./results.pmid.raw/" "./"
+bash doTextmine.sh "./pmid/pubmed20n" "./results.pmid.raw/" "./"
